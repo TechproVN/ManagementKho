cc54d7ddff2a010485bfcc6e0737dced4514ee5e
VuHoangTechpro <39793625+VuHoangTechpro@users.noreply.github.com>
2018-08-31 15:51:58 +0700

    api

---
+++ GetDataProduct.php
@@ -0,0 +1,24 @@
+<?php
+	$link = mysqli_connect("192.168.1.25", "vuhoang", "06111987", "warehouse");
+	/* check connection */
+	if (mysqli_connect_errno()) 
+	{
+		printf("Connect failed: %s\n", mysqli_connect_error());
+		exit();
+	}
+	$query = "CALL proc_GetDataProduct()";
+	//echo $query;
+	$rows = array();
+	if ($result = mysqli_query($link, $query)) 
+	{
+		while ($row = mysqli_fetch_assoc($result)) 
+		{
+			$rows[] = $row;
+		}
+		/* free result set */
+		mysqli_free_result($result);
+	}
+	/* close connection */
+	mysqli_close($link);
+	echo json_encode($rows);
+?>
